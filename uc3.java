3aa7c0b3e3988950f6d7c07379091c7706c64871
sk6881 <sk6881@srmist.edu.in>
2026-03-02 08:55:15 +0530

    Create uc3.java

---
+++ uc3.java
@@ -0,0 +1,12 @@
+public class oopsuc3 {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "  * ", "   * ", "  ** ", "  ** "));
+        System.out.println(String.join(" ", " * * ", " * * ", " * * ", "*   *"));
+        System.out.println(String.join(" ", " * * ", " * * ", " * * ", "** "));
+        System.out.println(String.join(" ", " * * ", " * * ", " ***", "   * "));
+        System.out.println(String.join(" ", " * * ", " * * ", " * ", "       *"));
+        System.out.println(String.join(" ", " * * ", " * * ", " * ", "  *   ** "));
+        System.out.println(String.join(" ", "  * ", "   * ", "  * ", "   ***"));
+    }
+
+}
